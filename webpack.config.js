0c7fce80a341b3065673b3b01e0cb829630d947f
ao-spirogyra <lily.spirogyra@gmail.com>
2021-05-17 10:14:09 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.ts',
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, 'dist'),
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modeules/,
+        use: [
+          'ts-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+}
