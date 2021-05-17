4138f535be343d09505df62f1d2c0ebdc03c70e4
takumimunemura <lakmrs.2525.tm@gmail.com>
2021-05-17 21:55:32 +0900

    ボタンをいいかんじにした

--- src/ts/BookViewer.tsx
+++ src/ts/BookViewer.tsx
@@ -6,30 +6,51 @@ export interface BookViewer {
 
 export const BookViewer: React.FC<BookViewer> = ({pages}: BookViewer) => {
   const [currentPage, setCurrentPage] = useState(0)
-  const [disable, setDisable] = useState(false)
+  const [lastPage, setLastPage] = useState(false)
+  const [firstPage, setFirstPage] = useState(true)
 
   const checkLastPage = (page) => {
     if (page === pages.length-1) {
-      setDisable(true)
+      setLastPage(true)
     } else {
-      setDisable(false)
+      setLastPage(false)
+    }
+  }
+  const checkFirstPage = (page) => {
+    if (page === 0) {
+      setFirstPage(true)
+    } else {
+      setFirstPage(false)
     }
   }
 
   const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCurrentPage(parseInt(e.target.value))
     checkLastPage(parseInt(e.target.value))
+    checkFirstPage(parseInt(e.target.value))
   }
-  const onClickButton = () => {
+  const nextPage = () => {
       const newPage = currentPage +1
       setCurrentPage(newPage)
+      checkLastPage(newPage)
+      checkFirstPage(newPage)
+  }
+  const backPage = () => {
+      const newPage = currentPage -1
+      setCurrentPage(newPage)
+      checkFirstPage(newPage)
       checkLastPage(newPage)
   }
   
   return (
     <div className='container-book-viewer'>
-      <button className="add-page" onClick={onClickButton} disabled={disable}>aaaaaaaaaaaaaa</button>
-      <img className='image' src={pages[currentPage]} alt="" />
+      <div className="image-box">
+        <img className='image' src={pages[currentPage]} alt="" />
+        <div className="page-buttons">
+          <button className="next-page-button" onClick={nextPage} disabled={lastPage}><div className="text">aaaaaaaaaaaaaa</div></button>
+          <button className="back-page-button" onClick={backPage} disabled={firstPage}><div className="text">aaaaaaaaaaaaaa</div></button>
+        </div>
+      </div>
       <div className='tooltip-bar'>
         <input className='input' type="range" min={0} max={pages.length-1} defaultValue={0} onChange={onChange} value={currentPage}/>
       </div>
