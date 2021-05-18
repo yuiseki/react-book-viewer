a37303f193d98c0ad5ec6d1502de1d07c89d45f1
ao-spirogyra <lily.spirogyra@gmail.com>
2021-05-19 05:57:48 +0900

    create header for development

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -2,7 +2,27 @@
 import { render } from 'react-dom'
 import { BookViewer } from '../../src/ts/BookViewer'
 import { useImage } from './hooks/useImage'
+import './index.css'
 
 const images = useImage()
-const App = () => <BookViewer pages={images}/>;
+const offSetHeight = '80px'
+const GlobalHeader: React.FC = () => {
+  return (
+      <>
+     　<div className="header" style={{height: offSetHeight}}>
+          <div className="mini">
+          </div>
+          <a id="link" href='/'>&gt; top page</a>
+          <span id="arrow"></span><a id="link" href='/aboutus'>&gt; about us</a>
+          <span id="arrow"></span><a id="link" href='/contactus'>&gt; contact us</a>
+          <span id="arrow"></span><a id="link" href='/joinus'>&gt; join us</a>
+       </div>
+      </>
+  )
+}
+const children = {
+  Render: GlobalHeader,
+  height: offSetHeight
+}
+const App = () => <BookViewer pages={images} children={children}/>;
 render(<App />, document.getElementById('root'));
